5b35a60bed206c6370506c192c9aafc0424c7323
Renzo Crisostomo <ruenzuo@gmail.com>
2014-08-17 14:31:37 +0200

    Initial commit

---
+++ app/src/main/java/com/ruenzuo/babel/activities/MenuActivity.java
@@ -0,0 +1,17 @@
+package com.ruenzuo.babel.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.ruenzuo.babel.R;
+
+
+public class MenuActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_activity_layout);
+    }
+    
+}
